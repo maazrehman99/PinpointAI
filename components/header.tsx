46f471269c0e27650d241fa35062ec8d6a05edc3
Maaz Rehman <maaz97965@gmail.com>
2024-10-27 14:35:17 +0500

    Fix: Ui errors

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 import { ThemeToggle } from '@/components/theme-toggle'
 import { Button } from '@/components/ui/button'
+import MeetingTaskAnalyzer from '@/app/analyzer/meeting/page'
 
 export default function Header() {
   return (
@@ -10,10 +11,8 @@ export default function Header() {
       <nav className='container flex items-center justify-between'>
         <ul className='flex gap-10 text-sm font-medium'>
           <li>
-            <Link href='/'>Meeting Task Analyzer</Link>
+            <Link href='/analyzer/meeting'>PinPoint AI</Link>
           </li>
-         
-        
       
         </ul>
 
@@ -30,6 +29,7 @@ export default function Header() {
           </SignedIn>
         </div>
       </nav>
+      {/* <MeetingTaskAnalyzer/> */}
     </header>
   )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/protected(.*)', '/forum(.*)'])
+const isProtectedRoute = createRouteMatcher(['/analyzer(.*)', '/forum(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth()
